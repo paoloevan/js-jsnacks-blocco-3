609f6c586c4a76fa84a6b5dab48e88e02017a5d9
paoloevan <paoloev1900@gmail.com>
2022-10-21 11:30:35 +0200

    add function

---
+++ jsnack1/main.js
@@ -0,0 +1,65 @@
+/*
+Snack 1:
+Crea un array composto da 10 automobili.
+Ogni oggetto automobile avrà le seguenti proprietà:
+marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina,
+nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+Infine stampa separatamente i 3 array.
+*/
+
+const cars = [
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'benzina'
+    },
+    {
+        marca: 'fiat',
+        modello: 'punto',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'fiat',
+        modello: 'idea',
+        alimentazione: 'metano'
+    },
+    {
+        marca: 'volkswagen',
+        modello: 'polo',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'volkswagen',
+        modello: 'golf',
+        alimentazione: 'benzina'
+    },
+    {
+        marca: 'volkswagen',
+        modello: 'lupo',
+        alimentazione: 'gpl'
+    },
+    {
+        marca: 'volkswagen',
+        modello: 'polo',
+        alimentazione: 'benzina'
+    },
+    {
+        marca: 'peugeut',
+        modello: '206',
+        alimentazione: 'benzina'
+    },
+    {
+        marca: 'peugeut',
+        modello: '208',
+        alimentazione: 'metano'
+    },
+    {
+        marca: 'peugeut',
+        modello: '2008',
+        alimentazione: 'elettrico'
+    }
+]
+
+const benzina = cars.filter((marca) => cars.alimentazione == 'benzina')
+console.log(benzina);
